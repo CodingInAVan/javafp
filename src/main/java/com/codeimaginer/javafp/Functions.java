9eadafc6ff9dd0867dff04195a455830e0e4bf5b
mjjmmjj <mjjmmjj@gmail.com>
2019-09-01 08:14:16 -0500

    Adding compose functions

--- src/main/java/com/codeimaginer/javafp/Functions.java
+++ src/main/java/com/codeimaginer/javafp/Functions.java
@@ -10,4 +10,12 @@ public class Functions {
     Function<Function<U, V>, Function<T, V>>> higherAndThen() {
     return (Function<T, U> f) -> (Function<U, V> g) -> (T x) -> g.apply(f.apply(x));
   }
+
+  static <T, U, V> Function<V, U> compose(Function<T, U> f1, Function<V, T> f2) {
+    return (V arg) -> f1.apply(f2.apply(arg));
+  }
+
+  static <T, U, V> Function<V, U> composeAndThen(Function<V, T> f1, Function<T, U> f2) {
+    return (V arg) -> f2.apply(f1.apply(arg));
+  }
 }
--- src/test/java/com/codeimaginer/javafp/FunctionsTest.java
+++ src/test/java/com/codeimaginer/javafp/FunctionsTest.java
@@ -19,4 +19,12 @@ public class FunctionsTest {
     Integer x = Functions.<Integer, Integer, Integer> higherAndThen().apply(square).apply(triple).apply(10);
     Assertions.assertEquals(300, x);
   }
+
+  @Test
+  public void composeTest() {
+    Double v1 = Functions.compose(z -> z / 10, Math::sin).apply(20.0);
+    Assertions.assertEquals(Math.sin(20.0) / 10, v1);
+    Double v2 = Functions.<Double, Double, Double>composeAndThen(z -> z / 10, Math::sin).apply(20.0);
+    Assertions.assertEquals(Math.sin(20.0 / 10), v2);
+  }
 }
